a5221ae168b04a17739def55926d80af4d0437e4
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2019-06-25 17:45:14 -0700

    nav in progress

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,84 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { GoMarkGithub } from 'react-icons/go'
 import { baseFont } from './colors'
+import { css } from '@emotion/core'
+
+// Nav button based on:
+// https://css-tricks.com/responsive-menu-concepts/
+// and https://codepen.io/erikterwan/pen/EVzeRP
+
+const hamburgerCSS = css`
+  padding-top: 0em;
+  margin: 0;
+`
+
+const menuToggle = css`
+  user-select: none;
+  position: relative;
+  z-index: 1;
+  input {
+    /* opacity: 0; /* hide this */
+    position: absolute;
+    z-index: 2; /* and place it over the hamburger */
+    width: 3em;
+    height: 3em;
+    top: -1em;
+  }
+
+  /*
+ * Just a quick hamburger
+ */
+  span {
+    display: block;
+    width: 33px;
+    height: 4px;
+    margin-bottom: 5px;
+    position: relative;
+
+    background: #cdcdcd;
+    border-radius: 3px;
+
+    z-index: 1;
+
+    transform-origin: 4px 0px;
+
+    transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
+      background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
+  }
+
+  span:first-child {
+    transform-origin: 0% 0%;
+  }
+
+  span:nth-last-child(2) {
+    transform-origin: 0% 100%;
+  }
+
+  /* 
+ * Transform all the slices of hamburger
+ * into a crossmark.
+ */
+  input:checked ~ span {
+    opacity: 1;
+    transform: rotate(45deg) translate(-2px, -1px);
+    background: #232323;
+  }
+
+  /*
+ * But let's hide the middle one.
+ */
+  input:checked ~ span:nth-last-child(3) {
+    opacity: 0;
+    transform: rotate(0deg) scale(0.2, 0.2);
+  }
+
+  /*
+ * Ohyeah and the last one should go the other direction
+ */
+  input:checked ~ span:nth-last-child(2) {
+    transform: rotate(-45deg) translate(0, -1px);
+  }
+`
 
 const Header = ({ siteTitle }) => (
   <header
@@ -35,24 +113,34 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-      <a
-        href="https://github.com/hanhanhan/"
-        style={{
-          padding: `0`,
-          margin: `0`,
-          display: `flex`,
-          alignItems: `center`,
-        }}
-      >
-        <GoMarkGithub
-          size="3em"
-          color="salmon"
-          style={{
-            padding: `0`,
-            margin: `0`,
-          }}
-        />
-      </a>
+
+      <nav css={menuToggle}>
+        <input type="checkbox" />
+        {/* For CSS Hamburger */}
+        <span />
+        <span />
+        <span />
+        <ul>
+          <li>Github</li>
+          <li>LinkedIn</li>
+          <li>Work History</li>
+          <li>Projects</li>
+          <li>
+            {' '}
+            <a
+              href="https://github.com/hanhanhan/"
+              style={{
+                padding: `0`,
+                margin: `0`,
+                display: `flex`,
+                alignItems: `center`,
+              }}
+            >
+              <GoMarkGithub size="3em" color="salmon" css={hamburgerCSS} />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 )
